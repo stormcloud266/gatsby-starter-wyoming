adee2fd9d7008c834befc16a61ba9fa02ddbc0d8
Tawnee Allport <allprt89@gmail.com>
2020-09-21 09:38:37 -0600

    add name fields to inputs

--- src/components/form.js
+++ src/components/form.js
@@ -3,17 +3,17 @@ import React from "react"
 const Form = () => (
 
   <form name="contact" className="form" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-    
+
     <input type="hidden" name="bot-field" />
     <input type="hidden" name="form-name" value="contact" />
 
     <div className="form__section">
-      <input type="text" id="name"/>
+      <input type="text" id="name" name="name"/>
       <label htmlFor="name">Name</label>
     </div>
 
     <div className="form__section">
-      <input type="email" id="email"/>
+      <input type="email" id="email" name="email"/>
       <label htmlFor="email">Email</label>
     </div>
 
