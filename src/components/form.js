a202ee5e28bd75cfeef3d8afd4e9d5e07ab405aa
Tawnee Allport <allprt89@gmail.com>
2020-09-21 09:08:43 -0600

    fix plugin manifest path. initialize netlify form

--- src/components/form.js
+++ src/components/form.js
@@ -1,7 +1,11 @@
 import React from "react"
 
 const Form = () => (
-  <form action="" className="form">
+
+  <form name="contact" className="form" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+    
+    <input type="hidden" name="bot-field" />
+    <input type="hidden" name="form-name" value="contact" />
 
     <div className="form__section">
       <input type="text" id="name"/>
