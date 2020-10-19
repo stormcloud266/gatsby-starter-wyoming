08d2461dc3d7c98ea22a6bfb17024b71a2d2f0f4
Tawnee Allport <allprt89@gmail.com>
2020-10-18 18:02:52 -0600

    change name in manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,11 +68,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-starter-wyoming`,
+        short_name: `wyoming`,
         start_url: `/`,
-        background_color: `##222021`,
-        theme_color: `##222021`,
+        background_color: `#222021`,
+        theme_color: `#222021`,
         display: `minimal-ui`,
         icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
