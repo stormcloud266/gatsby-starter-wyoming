327134f7c30035368b4823636aa160a1aad5ed99
Tawnee <allprt89@gmail.com>
2021-09-09 15:07:20 -0600

    style footer

--- src/components/global/footer/footer.js
+++ src/components/global/footer/footer.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import NavLinks from '@global/navLinks'
+import { Container, Flex } from '@UI'
 import * as styles from './footer.module.scss'
 
 const Footer = () => (
 	<footer className={styles.footer}>
-		<div className='wrapper'>
-			<nav className={styles.nav}>
-				<NavLinks />
-			</nav>
-		</div>
+		<Container wrapper section>
+			<Flex autoLayout collapseOnMd className={styles.flex}>
+				<div>
+					<p className={styles.logo}>Logo</p>
+					<p className={styles.body}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+						obcaecati vitae consectetur.
+					</p>
+				</div>
+				<nav className={styles.nav}>
+					<NavLinks />
+				</nav>
+			</Flex>
+		</Container>
 	</footer>
 )
 
