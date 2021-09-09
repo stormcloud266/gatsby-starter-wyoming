88d9dde00afa0d0dd9fad287c0de071ce510b6e8
Tawnee <allprt89@gmail.com>
2021-09-09 14:21:41 -0600

    add hero button. adjust button styles

--- src/components/home/hero/hero.js
+++ src/components/home/hero/hero.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
-import { Container, Title } from '@UI'
+import { Fade } from 'react-awesome-reveal'
+import { Container, Title, Button } from '@UI'
 import * as styles from './hero.module.scss'
 
 const Hero = () => {
@@ -18,9 +19,16 @@ const Hero = () => {
 					style={{ position: 'static' }}
 				/>
 			</div>
-			<Title tag='h1' shadow className={styles.title}>
-				Wyoming
-			</Title>
+			<div className={styles.content}>
+				<Title tag='h1' shadow className={styles.title}>
+					Wyoming
+				</Title>
+				<Fade triggerOnce duration={500} delay={500}>
+					<Button cta to='/#contact'>
+						Contact Us Today
+					</Button>
+				</Fade>
+			</div>
 		</Container>
 	)
 }
