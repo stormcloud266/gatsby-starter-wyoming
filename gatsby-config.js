5794d8c0655f4a6da5159a683e94efa3e46bce35
Tawnee <allprt89@gmail.com>
2022-03-15 14:24:21 -0600

    add robots txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
 		'gatsby-plugin-image',
 		'gatsby-plugin-react-helmet',
 		'gatsby-plugin-sitemap',
+		'gatsby-plugin-robots-txt',
 		{
 			resolve: 'gatsby-plugin-manifest',
 			options: {
